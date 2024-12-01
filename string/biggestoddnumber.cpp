234b17269ec17d0cd7fb4524075442deea742a2e
Chitresh Gurjar <chitresh.gurjar8779@gmail.com>
2024-12-01 18:25:46 +0530

    update

--- string/biggestoddnumber.cpp
+++ string/biggestoddnumber.cpp
@@ -4,19 +4,19 @@ int main(){
     string s;
     cin>>s;
     int n=s.length();
-    int i=n-1;
+    int i=n-1,d=0;
 
     while(i>=0){
         if(s[i]=='0'||s[i]=='2'||s[i]=='4'||s[i]=='6'||s[i]=='8'){
             continue;
         }
         else{
-            n=i;
+            d=i;
             break;
         }
         i--;
     }
-    for(int j=0;j<=n;j++){
+    for(int j=0;j<=d;j++){
         cout<<s[j];
     }
     return 0;
