dec971e31556f3a4d6085a17c8673be18da49d0e
Chitresh Gurjar <chitresh.gurjar8779@gmail.com>
2024-12-04 20:14:35 +0530

    merge sort changes

--- array/mergesort.cpp
+++ array/mergesort.cpp
@@ -2,11 +2,14 @@
 using namespace std;
 
 int mergesort(int arr[],int low,int high){
+    if(low==high){
+        return ;
+    }
     
 }
  
 int main(){
-    int arr,n,h;
+    int arr[5]={},n,h;
     mergesort(arr,n,h);
     return 0;
 }
